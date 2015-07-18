2c157247772ac0d5371c23b34607246f96962ca1
Sahit Mandala <smandala@cs-it-3.local>
2015-07-18 16:40:56 -0700

    added js files

---
+++ javascript/app-config.js
@@ -0,0 +1,10 @@
+var config = {
+   appconfig: [
+       {url:”n/meaningcloud", sessid:null}
+   ],
+   initWithData: false,
+   startIndex: 0,
+   autoLogin: true,
+   host: "portal.nimblestack.io",
+   protocol: "https"
+};
