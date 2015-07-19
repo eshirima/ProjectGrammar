ba84c24b76850b2b72dec6b0ec96bb5fcbd5eb10
Sahit Mandala <smandala@cs-it-3.local>
2015-07-18 20:33:34 -0700

    more

--- js/main.js
+++ js/main.js
@@ -18,7 +18,26 @@ _app.formSubmission = function (event) {
 
 _app.formSubmissionHandler = function (data){
    var json = JSON.parse(data);
+   var pages = json['ListPage']
     
 }
+
+_app.audioSubmission = function (event) {
+   event.preventDefault();
+   var val = document.getElementById("inputarea").value;
+   var params = [
+       {'name': 'key', value:'810ff583bed6266bc7cec7781b49cfc9'},
+       {'name': 'txt', value:val}
+   ];
+    console.log("Got Submission");
+    _app.nimble.get(0, _app.audioSubmissionHandler, true, params);
+}
+
+_app.audioSubmissionHandler = function (data){
+   var json = JSON.parse(data);
+   var pages = json['ListPage']
+    
+}
+
 
 _app.initialize();
